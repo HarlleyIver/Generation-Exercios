b36b9c1afb9b696db6b9f25132b3943d33f0305d
HarlleyIver <130095726+HarlleyIver@users.noreply.github.com>
2023-08-08 22:40:14 -0300

    Add files via upload

---
+++ Exercicio02.java
@@ -0,0 +1,31 @@
+package Collections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       Scanner leia = new Scanner(System.in);
+	        Set<Integer> numeros = new HashSet<>();
+
+	        for(int i=0;i<10;i++) {
+	            System.out.printf("Digite o %dº número: ", i+1);
+	            int numero = leia.nextInt();
+	            numeros.add(numero);
+	        }
+
+	        Iterator<Integer> inumeros = numeros.iterator();
+	        System.out.println("Lista dos dados do Set: ");
+	        while(inumeros.hasNext()){
+	            System.out.println(inumeros.next());
+	        }
+	    }
+	
+
+	
+
+}
